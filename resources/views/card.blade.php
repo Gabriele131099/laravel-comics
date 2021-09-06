cf1ba4489a037be620f8eeff5a7cfd2493e10e63
gabriele131099 <gabrisanna131099@gmail.com>
2021-09-06 19:31:32 +0200

    php

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -34,20 +34,22 @@
                 <img src="{{$comics[$id]['thumb']}}" alt="img pubblicità">
             </div>
         </div>
+        <div style="background-color:lightgrey">
         <div class="container list-table">
             <ul>
-                <li>Talent
-                    <ul class="list">
-                        <li>Art by:</li>
-                        <li>
+                <li><div>Talent</div>
+                    <hr>
+                    <ul>
+                        <li class="titleTable">Art-by:</li>
+                        <li class="textArtists">
                            @foreach($comics[$id]['artists'] as $comicArtist)
                             {{$comicArtist}} ,
                            @endforeach 
                         </li>
                     </ul>
-                    <ul class="list">
-                        <li>Written by:</li>
-                        <li>
+                    <ul>
+                        <li class="titleTable">Written by:</li>
+                        <li class="textWriters">
                         
                         @foreach($comics[$id]['writers'] as $comicWriter)
                             {{$comicWriter}} ,
@@ -59,21 +61,23 @@
                 </li>
             </ul>
             <ul>
-                <li>Specs
-                    <ul class="list">
-                        <li>Series</li>
+                <li><div>Specs</div>
+                    <hr>
+                    <ul class="titleTable">
+                        <li class="titleTable">Series</li>
                         <li>{{$comics[$id]['series']}}</li>
                     </ul>
-                    <ul class="list">
-                        <li>U.S Price</li>
+                    <ul class="titleTable">
+                        <li class="titleTable">U.S Price</li>
                         <li>{{$comics[$id]['price']}}</li>
                     </ul>
-                    <ul class="list">
-                        <li>On sala date</li>
+                    <ul class="titleTable">
+                        <li class="titleTable">On sala date</li>
                         <li>{{$comics[$id]['sale_date']}}</li>
                     </ul>
                 </li>
             </ul>
+        </div>
         </div>
     </div>
 @endsection
